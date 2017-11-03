084c24d915f4ca5f15b08694bfe259253c9ec5ed
Grant Carthew <grant@carthew.net>
2017-11-03 17:49:29 +1000

    Added functions and expanded tests

--- index.js
+++ index.js
@@ -1,34 +1,135 @@
+class Is {
+  string (value) {
+    return Object.prototype.toString.call(value) === '[object String]'
+  }
+
+  number (value) {
+    return Object.prototype.toString.call(value) === '[object Number]'
+  }
+
+  nan (value) {
+    return Number.isNaN(value)
+  }
+
+  integer (value) {
+    return this.number(value) && !this.nan(value) && value % 1 === 0
+  }
+
+  alphaNum (value) {
+    return value.match(/[^0-9a-z]/i) === null
+  }
+}
+
+class MppgError extends Error {
+  constructor (...params) {
+    super(...params)
+    Error.captureStackTrace(this, MppgError)
+    this.date = new Date()
+  }
+  static throw (...params) {
+    throw new MppgError(...params)
+  }
+  static get messages () {
+    return Object.freeze({
+      invalidChr: 'Invalid characters in the path or id string. Only 0-9 and A-Z allowed.',
+      invalidPathLength: 'Path length not related to pad length.',
+      intNan: 'Path id is not a number (NaN).',
+      intMax: 'Path id value is greater than the maximum permitted with the current padLength.',
+      pathIdMin: 'Path id value is zero which is below the starting value of one.',
+      noParent: 'Path id does not have a parent.'
+
+    })
+  }
+}
+
 class MPPG {
   constructor (options) {
     this.padLength = options.padLength
-    this.padChr = '0'
+    this.minChr = '0'
+    this.maxChr = 'Z'
+    this.is = new Is()
+    this.errId = this.minChr.padStart(this.padLength, this.minChr)
+    this.minId = '1'.padStart(this.padLength, this.minChr)
+    this.maxId = this.maxChr.padStart(this.padLength, this.maxChr)
+    this.maxInt = this.fromBase36(this.maxId)
+    this.MppgError = MppgError
   }
 
-  toBase36 (value) {
-    return value.toString(36).toUpperCase()
+  cleanStr (strValue, checkLength = true) {
+    strValue = strValue || ''
+    strValue = this.is.string(strValue) ? strValue : strValue.toString()
+    this.is.alphaNum(strValue) || MppgError.throw(MppgError.messages.invalidChr, strValue)
+    checkLength &&
+      strValue.length % this.padLength !== 0 &&
+      MppgError.throw(MppgError.messages.invalidPathLength)
+    return strValue.toUpperCase()
   }
 
-  fromBase36 (value) {
-    return parseInt(value.toUpperCase(), 36)
+  cleanInt (intValue) {
+    intValue = this.is.integer(intValue) ? intValue : parseInt(intValue)
+    this.is.nan(intValue) && MppgError.throw(MppgError.messages.intNan)
+    intValue = Math.abs(intValue)
+    intValue > this.maxInt && MppgError.throw(MppgError.messages.intMax)
+    return intValue
   }
 
-  getNextId (value) {
-    let valueAsInt = value ? this.fromBase36(value) : 0
-    let newId = this.toBase36(++valueAsInt)
-    return newId.padStart(this.padLength, this.padChr)
+  toBase36 (intValue) {
+    intValue = this.cleanInt(intValue)
+    return intValue.toString(36).toUpperCase()
   }
 
-  getPreviousId (value) {
-    let valueAsInt = this.fromBase36(value)
-    return this.toBase36(--valueAsInt)
+  fromBase36 (strValue) {
+    strValue = this.cleanStr(strValue, false)
+    return parseInt(strValue, 36)
   }
 
-  getNextChildPath (path) {
-    let childPath = path
-
+  getPathLength (path) {
+    this.cleanStr(path)
+    return path.length === 0 ? 0 : path.length / this.padLength
   }
-  decode () {
 
+  testPathHasParent (mpath) {
+    mpath = this.cleanStr(mpath)
+    return mpath.length >= (2 * this.padLength)
+  }
+
+  getNextId (pathId) {
+    pathId = pathId ? this.fromBase36(pathId) : 0
+    pathId = this.toBase36(++pathId)
+    pathId = pathId.padStart(this.padLength, this.minChr)
+    return pathId
+  }
+
+  getPreviousId (pathId) {
+    pathId = this.fromBase36(pathId)
+    pathId = this.toBase36(--pathId)
+    pathId = pathId.padStart(this.padLength, this.minChr)
+    pathId === this.errId && MppgError.throw(MppgError.messages.pathIdMin)
+    return pathId
+  }
+
+  getParentId (mpath) {
+    mpath = this.cleanStr(mpath)
+    let twoPad = this.padLength * 2
+    mpath.length < twoPad && MppgError.throw(MppgError.messages.noParent)
+    return mpath.slice(-twoPad, -this.padLength)
+  }
+
+  getChildId (mpath) {
+    mpath = this.cleanStr(mpath)
+    mpath = mpath.length < this.padLength ? this.minId : mpath
+    if (mpath.length === this.padLength) { return mpath }
+    return mpath.slice(-this.padLength)
+  }
+
+  getNextChildPath (mpath) {
+    mpath = this.cleanStr(mpath)
+    return mpath + this.minId
+  }
+
+  getNextSibligPath (mpath) {
+    mpath = this.cleanStr(mpath)
+    if (mpath.length === this.padLength) { return }
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,24 +1,89 @@
 const MPPG = require('./index')
 const mppg = new MPPG({padLength: 5})
-const testString = 'ZZZZZ'
-const testInt = 60466175
+const mgs = mppg.MppgError.messages
+const lastId = 'ZZZZZ'
+const midId = 'X23GP'
+const midp1Id = 'X23GQ'
+const midm1Id = 'X23GO'
 const firstId = '00001'
+const maxInt = 60466175
+
+test('cleanStr', () => {
+  expect(mppg.cleanStr()).toBe('')
+  expect(mppg.cleanStr(12345)).toBe('12345')
+  expect(mppg.cleanStr('abc12')).toBe('ABC12')
+  expect(() => { mppg.cleanStr('abc123') }).toThrow(mgs.invalidPathLength)
+  expect(mppg.cleanStr('abc123', false)).toBe('ABC123')
+})
+
+test('cleanInt', () => {
+  // expect(mppg.cleanInt()).toBe(1)
+  expect(mppg.cleanInt(55)).toBe(55)
+  expect(mppg.cleanInt(0)).toBe(0)
+  expect(mppg.cleanInt(-1000)).toBe(1000)
+  expect(mppg.cleanInt('123abc')).toBe(123)
+  expect(mppg.cleanInt(123.456)).toBe(123)
+  expect(() => { mppg.cleanInt('#%4kjd') }).toThrow(mgs.intNan)
+})
 
 test('fromBase36', () => {
-  expect(mppg.fromBase36(testString)).toBe(testInt)
+  expect(mppg.fromBase36(lastId)).toBe(maxInt)
+  expect(mppg.fromBase36(1)).toBe(1)
 })
 
 test('toBase36', () => {
-  expect(mppg.toBase36(testInt)).toBe(testString)
+  expect(mppg.toBase36(maxInt)).toBe(lastId)
+})
+
+test('getPathLength', () => {
+  let pathLength = mppg.getPathLength(firstId + lastId)
+  expect(pathLength).toBe(2)
+  pathLength = mppg.getPathLength(firstId + midId + lastId)
+  expect(pathLength).toBe(3)
+  expect(() => { mppg.getPathLength('123') }).toThrow(mgs.invalidPathLength)
+  expect(() => { mppg.getPathLength('123ABC') }).toThrow(mgs.invalidPathLength)
+})
+
+test('testPathHasParent', () => {
+  expect(mppg.testPathHasParent()).toBe(false)
+  expect(mppg.testPathHasParent(firstId)).toBe(false)
+  expect(mppg.testPathHasParent(firstId + firstId)).toBe(true)
+  expect(mppg.testPathHasParent(firstId + firstId + firstId)).toBe(true)
 })
 
 test('getNextId', () => {
-  const nextId = mppg.getNextId(testString)
-  expect(nextId).toBe('100000')
+  expect(mppg.getNextId(firstId)).toBe('00002')
+  expect(mppg.getNextId(midId)).toBe(midp1Id)
   expect(mppg.getNextId()).toBe(firstId)
+  expect(() => { mppg.getNextId(lastId) }).toThrow(mgs.intMax)
 })
 
 test('getPreviousId', () => {
-  const nextId = mppg.getPreviousId(testString)
-  expect(nextId).toBe('ZZZZY')
+  expect(mppg.getPreviousId('00002')).toBe(firstId)
+  expect(mppg.getPreviousId(midId)).toBe(midm1Id)
+  expect(mppg.getPreviousId(lastId)).toBe('ZZZZY')
+  expect(() => { mppg.getPreviousId(firstId) }).toThrow(mgs.pathIdMin)
+})
+
+test('getParentId', () => {
+  expect(mppg.getParentId(firstId + midId + lastId)).toBe(midId)
+  expect(mppg.getParentId(firstId + midId)).toBe(firstId)
+  expect(() => { mppg.getParentId(firstId) }).toThrow(mgs.noParent)
+  expect(() => { mppg.getParentId() }).toThrow(mgs.noParent)
+})
+
+test('getChildId', () => {
+  expect(mppg.getChildId()).toBe(firstId)
+  expect(mppg.getChildId(firstId)).toBe(firstId)
+  expect(mppg.getChildId(firstId + midId)).toBe(midId)
+  expect(mppg.getChildId(firstId + midId + lastId)).toBe(lastId)
+})
+
+test('getNextChildPath', () => {
+  let nextChildPath = mppg.getNextChildPath()
+  expect(nextChildPath).toBe(firstId)
+  nextChildPath = mppg.getNextChildPath(midId)
+  expect(nextChildPath).toBe(midId + firstId)
+  nextChildPath = mppg.getNextChildPath(lastId + midId)
+  expect(nextChildPath).toBe(lastId + midId + firstId)
 })
